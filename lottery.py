9a4c6b65eb5c2964608fca78810cbdc16967a57b
25bekov <bomba.kg7856@gmail.com>
2023-02-18 18:35:51 +0600

    Add file

---
+++ lottery.py
@@ -0,0 +1,15 @@
+
+
+
+from random import choice
+lst = [i for i in range(1, 31)]
+def stavka(lot, amount):
+    win_lot = choice(lst)
+    if lot == win_lot:
+        print(f'выигранно {amount * 2}$ :)')
+        return amount * 2
+    print(f'проигранно {amount}$')
+    return -amount
+    if name == 'main':
+        print(lst)
+
